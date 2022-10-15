c6ed4c267d72ad193acc050e7531d31143220f41
ogulnur <hommaliyevaolya@gmail.com>
2022-10-15 13:14:48 +0500

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main">
+   <div class="header" v-if="path == '/'">
+    <Header />
+   </div>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/header.vue'
+import path from 'path';
+export default {
+  name: 'DefaultLayout',
+  components: {
+    Header,
+  },
+  data() {
+    return {}
+  },
+  computed:{
+    path(){
+      return this.$route.path;
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="landing">
+    <section class="home">
+      <div class="content">
+        <div class="left">
+          <nuxt-link to="" class="about_link">
+            About us
+          </nuxt-link>
+          <div class="text">
+            We will help you to orgonise your daily activities
+          </div>
+        </div>
+        <div class="right">
+          <div class="image">
+          <img src="./../assets/image/Frame 162.svg" alt="" />
+        </div>
+        <h1>Create daily routine</h1>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
